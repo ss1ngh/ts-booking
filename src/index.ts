cc0ebaf2dcc259a7b9a2b54c85f7fc7e5fee0fab
ss1ngh <singhshreyansh2354@gmail.com>
2026-03-12 15:17:21 +0530

    fix: implement modern Zod error treeification and global handler

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,30 @@ import type { Request, Response, NextFunction } from 'express';
 import v1Router from './routes/v1/index.js';
 import { StatusCodes } from 'http-status-codes';
 import { AppError } from './utils/AppError.js';
-import { ZodError } from 'zod';
+import z, { ZodError } from 'zod';
 
 const app = express();
 app.use(express.json());
 
 app.use('/' , v1Router);
 
-// 2. Global Error Handler (MUST be defined after all routes/middlewares)
+// 2. Global Error Handler
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     // Handle Zod Validation Errors
     if (err instanceof ZodError) {
+        const errorTree = z.treeifyError(err);
         return res.status(StatusCodes.BAD_REQUEST).json({
             success: false,
             message: "Validation Failed",
             data: {},
-            error: err.flatten().fieldErrors // Provides clean, readable errors
+            error: {
+                fieldErrors : 'properties' in errorTree ? errorTree.properties : {} ,
+                formErrors: errorTree.errors
+            } 
         });
     }
 
-    // Handle your custom AppErrors (like NotFoundError)
+    //handle custom AppErrors
     if (err instanceof AppError) {
         return res.status(err.statusCode).json({
             success: false,
@@ -32,8 +36,8 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
         });
     }
 
-    // Fallback for unexpected internal errors
-    console.error(err); // Log for debugging
+    //fallback for unexpected internal errors
+    console.error(err);
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         success: false,
         message: "Something went wrong",
